ac04606e0c5d47d0e73c6fe64584a0d601cb64a4
unknown <rosarioscarlata24@gmail.com>
2020-02-27 11:06:52 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import {request} from "./index.scss"
+import { request } from "./index.scss";
 import './index.scss';
+import "bootswatch/dist/cyborg/bootstrap.min.css";
 
 // const Title = document.getElementById('Title');
 
@@ -7,21 +8,37 @@ import './index.scss';
 
 // Aqui le decimos que busque peliculas que tengan el nombre Mario
 
-var tragos=document.getElementById("search");
- var buscador =document.getElementById("buscador");
- 
- function search() {
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${tragos.value}`).then(res => res.json())
-  .then(response => {console.log(response.drinks);
-      var bebidas =response.drinks 
-    console.log(bebidas)} 
-     )
- } 
+var tragos = document.getElementById("search");
+var buscador = document.getElementById("buscador");
+var contenido = document.querySelector('#contenido')
+
+function search() {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${tragos.value}`).then(res => res.json())
+        .then(response => {
+            // console.log(response.drinks);
+            var bebidas = response.drinks;
+            // console.table(bebidas);
+            tabla(bebidas)
+        });
+}
+
+function tabla(bebidas) {
+    console.table(bebidas);
+    contenido.innerHTML = '';
+    for (let valor of bebidas) {
+        contenido.innerHTML += ` <tr>
+        <th scope="row">${valor.strDrink}</th>
+        <td>${valor.strCategory}</td>
+        <td>${valor.strAlcoholic}</td>
+        <td>${valor.strGlass}</td>
+    </tr>`
+
+    }
+}
 
 
+buscador.addEventListener("click", function(e) {
+    search();
+    e.preventDefault();
 
- buscador.addEventListener("click",function (e) {
-   search();
-  e.preventDefault();
-   
- })
+});
--- src/requestHelper.js
+++ src/requestHelper.js
@@ -1,35 +1,34 @@
 export function request(url, callback) {
-  var data = null;
+    var data = null;
 
-  //Creamos el objeto request
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = false;
+    //Creamos el objeto request
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-  //Le decimos que hacer cuando tenga actualizaciones (esta es la parte asíncrona)
-  xhr.addEventListener("readystatechange", function() {
-    console.log('pepito');
-    if (this.readyState === this.DONE) {
-      // Aqui ejecutamos el callback y le inyectamos el valor que devuelve el servidor
-      callback(this.responseText);
-    }
-  });
+    //Le decimos que hacer cuando tenga actualizaciones (esta es la parte asíncrona)
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === this.DONE) {
+            // Aqui ejecutamos el callback y le inyectamos el valor que devuelve el servidor
+            callback(this.responseText);
+        }
+    });
 
-  // Configuramos a que url va a hacer la peticion (llega por parametro)
-  xhr.open(
-    "GET",
-    url
-  );
+    // Configuramos a que url va a hacer la peticion (llega por parametro)
+    xhr.open(
+        "GET",
+        url
+    );
 
-  // Hacemos set de requests para los permisos de la API
-  xhr.setRequestHeader(
-    "x-rapidapi-host",
-    "movie-database-imdb-alternative.p.rapidapi.com"
-  );
-  xhr.setRequestHeader(
-    "x-rapidapi-key",
-    "fb3009da76mshf35d0a642dad0ddp160f52jsna47d2842ea36"
-  );
-  
-  // Enviamos nuestro request al servidor
-  xhr.send(data);
+    // Hacemos set de requests para los permisos de la API
+    xhr.setRequestHeader(
+        "x-rapidapi-host",
+        "movie-database-imdb-alternative.p.rapidapi.com"
+    );
+    xhr.setRequestHeader(
+        "x-rapidapi-key",
+        "fb3009da76mshf35d0a642dad0ddp160f52jsna47d2842ea36"
+    );
+
+    // Enviamos nuestro request al servidor
+    xhr.send(data);
 }
